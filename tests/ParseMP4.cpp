e15860dcdc17704f69cc2b670821152a83bb2be4
LiangJiehu <0049003844@znv.com>
2025-03-13 00:49:54 +0800

    初步编写测试demo

--- tests/ParseMP4.cpp
+++ tests/ParseMP4.cpp
@@ -3,8 +3,8 @@
 
 
 
-int main() {
-	std::string filedata = File::loadFile("hy.mp4");
+int main(int argc, char* argv[]) {
+	std::string filedata = File::loadFile("test.mp4");
 	FileBox::Ptr root= std::make_shared<FileBox>();
 	MP4Buffer::Ptr stream = std::make_shared<MP4Buffer>();
 	stream->Append(filedata);
